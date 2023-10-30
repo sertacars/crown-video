ea16f7221d82f8fcefc415769e9abd715ea827bd
Sertac <sertacaras1@gmail.com>
2023-10-30 23:08:04 +0300

    Added get method for movies db

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,23 @@
 from typing import Union
 from fastapi import FastAPI
 
+import pymysql
+
 app = FastAPI()
+
+
+@app.get("/movies")
+async def get_movies():
+    """Get a list of all movies."""
+
+    connection = pymysql.connect(
+        host="localhost", user="root", password="", database="crownie_database"
+    )
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM movies")
+    movies = cursor.fetchall()
+
+    connection.close()
+
+    return movies
